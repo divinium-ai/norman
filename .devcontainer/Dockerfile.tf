ec78838127672e42b9f09d0c710d9b68697b8ed0
Gregory Bryant <bryagd@gmail.com>
2022-05-24 15:22:25 -0400

    adjusting permissions

--- .devcontainer/Dockerfile.tf
+++
@@ -1,7 +0,0 @@
-FROM tensorflow/tensorflow:latest-gpu-jupyter
-
-RUN groupadd --gid 5000 vscode \
-    && useradd --home-dir /home/tfuser --create-home --uid 5000 \
-    --gid 5000 --shell /bin/sh --skel /dev/null vscode
-
-ENV SHELL /bin/bash
