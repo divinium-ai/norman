a685bde98e513894026c8fbc52a6f61d622ebf81
Gregory Bryant <bryagd@gmail.com>
2022-05-24 14:40:41 -0400

    adding vscode dir

--- .devcontainer/Dockerfile.tf
+++ .devcontainer/Dockerfile.tf
@@ -1,7 +1,7 @@
 FROM tensorflow/tensorflow:latest-gpu-jupyter
 
-RUN groupadd --gid 5000 tfuser \
+RUN groupadd --gid 5000 vscode \
     && useradd --home-dir /home/tfuser --create-home --uid 5000 \
-    --gid 5000 --shell /bin/sh --skel /dev/null tfuser
+    --gid 5000 --shell /bin/sh --skel /dev/null vscode
 
 ENV SHELL /bin/bash
